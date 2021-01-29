1c02d6e828413967acdab3515e83ac26c3a6f17f
github username <github email address>
2021-01-28 21:53:57 -0500

    reducer work

--- src/reducers/bookReducer.js
+++ src/reducers/bookReducer.js
@@ -1,6 +1,13 @@
 
 export default function bookReducer(state = {books: []}, action ) {
 
+  debugger;
+  switch (action.type) {
+    case 'FETCH_BOOKS':
+      return state
+      default:
+      return state
+  }
 
   return state
 }
