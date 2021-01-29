906874a6ce32645777e26578880798e4eea7016c
github username <github email address>
2021-01-28 21:04:48 -0500

    add bokes to book input

---
+++ src/components/BookInput.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class BookInput extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+      Book Input
+      </div>
+    )
+  }
+}
+
+
+export default BookInput
---
+++ src/components/Books.js
@@ -0,0 +1,8 @@
+import react from 'react'
+
+
+
+
+
+
+//just function bc just presenting list of account
