37682fc269d221ffe171ee7eb50777faa965de76
github username <github email address>
2021-01-31 11:24:59 -0500

    initial state and values in book.js

--- src/components/BookInput.js
+++ src/components/BookInput.js
@@ -2,19 +2,22 @@ import React from 'react'
 
 class BookInput extends React.Component {
 
+  //can be constructor or can use state=
+
+  state = {title: '', genre: '', author: '', link: ''}
 
   render() {
     return (
       <div>
         <form>
           <label>Book or Article Title:</label>
-          <input type='text' placeholder='Title'/><br></br>
+          <input type='text' placeholder='Title' value={this.state.title}/><br></br>
           <label>Book Author:</label>
-          <input type='text' placeholder='Author'/><br></br>
+          <input type='text' placeholder='Author'value={this.state.author}/><br></br>
           <label>Book Genre:</label>
-          <input type='text' placeholder='Genre'/><br></br>
+          <input type='text' placeholder='Genre' value={this.state.genre}/><br></br>
           <label>Book Link:</label>
-          <input type='text' placeholder='only if applicable'/>
+          <input type='text' placeholder='only if applicable' value={this.state.link}/>
         </form>
       </div>
     )
