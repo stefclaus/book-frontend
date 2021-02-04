4196f11bc9ae5f3377522aae1f0daec60e94875b
github username <github email address>
2021-02-03 21:30:09 -0500

    add links to books.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import {Redirect} from 'react-router-dom'
 
 const Book = (props) => {
 
   console.log(props)
-  let book = props.books[props.match.params.id - 1]
+
+//  let book = props.books[props.match.params.id - 1]
 //  let book = props.books.filter(books => book.id == props.match.params.id)[0]
-  console.log(book)
   return (
-    <div>
-     {book ? book.title : null}
-   </div>
+    <li>
+      {/*book ? book.title : null*/}
+    </li>
   )
 
 
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Book from './Book'
+import {Route, Link} from 'react-router-dom'
 
 const Books = (props) => {
   return (
     <div>
       {props.books.map(book =>
-        <div key={book.id}><Book book={book} /></div> )}
+        <div key={book.id}>
+          <Link path={`/books/${book.id}`}>{book.author}</Link>
+        </div> )}
     </div>
   )
 
