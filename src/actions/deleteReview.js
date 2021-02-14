50d0e6bdf5c884ade5d88047d4a2e9ec6d78f953
github username <github email address>
2021-02-13 20:21:15 -0500

    delete_review reducer

--- src/actions/deleteReview.js
+++ src/actions/deleteReview.js
@@ -4,7 +4,8 @@ export const deleteReview = (reviewId, bookId) => {
     return fetch(`http://localhost:3000/api/v1/books/${bookId}/reviews/${reviewId}/`), {
       method: 'DELETE'
     })
-    .then(response => dispatch({type: 'DELETE_REVIEW', reviewId: reviewId}))
+    .then(reponse => response.json())
+    .then(book => dispatch({type: 'DELETE_REVIEW', payload: book}))
   }
 
 }
