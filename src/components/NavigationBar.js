a3f7f601682119cf083dd1c1f487114c75814915
github username <github email address>
2021-02-28 09:47:02 -0500

    change color theme, add newbook link

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
 import styled from 'styled-components';
 const Styles = styled.div`
-  .navbar { background-color: #8F3A84; }
+  .navbar { background-color: #de6fca; }
   a, .navbar-nav, .navbar-light .nav-link {
     color: #222;
     &:hover { color: white; }
@@ -22,11 +22,13 @@ export const NavigationBar = () => (
   <Styles>
     <Navbar expand="lg">
       <Navbar.Brand href="/books">Book Review</Navbar.Brand>
+      <Nav.Item><Nav.Link href="/books/new">Add A New Book</Nav.Link></Nav.Item>
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Item><Nav.Link href="/books">Home</Nav.Link></Nav.Item>
           <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+
         </Nav>
       </Navbar.Collapse>
     </Navbar>
